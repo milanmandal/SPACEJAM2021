7a48b44fed6c5b03521f6eef4bd418141151a41a
milanmandal <51320381+milanmandal@users.noreply.github.com>
2021-02-28 09:53:00 +0530

    final

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css"; //Bootstrap helful for CSS
 import { BrowserRouter as Router, Route,Switch} from "react-router-dom";
 import Cover from './component/home/cover/cover'
 import Encode from './component/home/conversion/encode'
+import Decode from './component/home/conversion/decode'
 export default function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ export default function App() {
         </div> 
       <Route path="/" exact component={Cover} />
       <Route path="/encode" exact component={Encode}/>
+      <Route path="/decode" exact component={Decode}/>
 </Router>
   )
 }
--- src/component/home/conversion/decode.js
+++ src/component/home/conversion/decode.js
@@ -4,34 +4,13 @@ import Logic from '../../logic/digitalToDNA'
 import {Link} from 'react-router-dom'
 let data='';
 export class convert extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            input:'',
-            val:true,
-        }
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChangeinput = this.onChangeinput.bind(this);
-      }
-      onChangeinput(e) {
-        this.setState({
-          input: e.target.value
-        })
-      }
-    onSubmit(e){
-        e.preventDefault();
-        this.setState({
-            val:false,
-        })    
-        console.log(this.state.val)
-        data=this.state.input;
-    }
+   
     render() {
         return (
-            <div className="in">
+            <div className="in2">
               The DNA strand after printing could be used to regain the information which was encoded in the strands and injected as a serum.
-
+                <br>
+                </br>
               <Link to={'/'}><button className="btn btn-success" >HOME</button></Link>       
             </div>
         )
